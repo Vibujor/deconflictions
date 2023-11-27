0e3378678d79a10a240c1d19878d46d73dae2d0c
Vibujor <enceladael@gmail.com>
2023-11-27 15:30:14 +0000

    comments + variable changes

--- functions_heuristic.py
+++ functions_heuristic.py
@@ -13,33 +13,43 @@ def predict_fp(
     flight: Flight,
     fp: FlightPlan,
     start: Union[str, pd.Timestamp],
-    # stop: str | pd.Timestamp,
     stop: Union[str, pd.Timestamp],
-    minutes: int = 15,  # REPLACE WITH INT
+    minutes: int = 15,
     angle_precision: int = 2,
     min_distance: int = 150,
 ) -> Flight:
+    """
+    Predicts trajectory based on the corresponding flight plan.
+
+    :param flight: Flight of interest
+    :param fp: Flight plan corresponding to flight
+    :param start: Start of deviation
+    :param stop: Stop of deviation
+    :param minutes: Number of minutes to predict
+    :param angle_precision: Angle precision for alignment to navaids
+    :param min_distance: Distance from which we consider a navpoint for alignment
+    """
     data_points: Dict[str, List[Union[float, str, pd.Timestamp]]] = {
         "latitude": [],
         "longitude": [],
         "timestamp": [],
     }
-    # print(f"start = {start}\nstop = {stop}\n")
+
     assert flight is not None
-    trou = flight.between(start, stop, strict=False)
-    assert trou is not None
-    section = cast(Flight, flight.before(trou.start, strict=False)).last(minutes=20)
+    hole = flight.between(start, stop, strict=False)
+    assert hole is not None
+    section = cast(Flight, flight.before(hole.start, strict=False)).last(minutes=20)
     assert section is not None
     gs = section.groundspeed_mean * 0.514444  # conversion to m/s
 
     data_points["latitude"].append(
-        cast(Position, cast(Flight, flight.before(trou.start)).at_ratio(1)).latitude
+        cast(Position, cast(Flight, flight.before(hole.start)).at_ratio(1)).latitude
     )
     data_points["longitude"].append(
-        cast(Position, cast(Flight, flight.before(trou.start)).at_ratio(1)).longitude
+        cast(Position, cast(Flight, flight.before(hole.start)).at_ratio(1)).longitude
     )
     data_points["timestamp"].append(
-        cast(Position, cast(Flight, flight.before(trou.start)).at_ratio(1)).timestamp
+        cast(Position, cast(Flight, flight.before(hole.start)).at_ratio(1)).timestamp
     )
 
     navaids = fp.all_points
@@ -55,14 +65,14 @@ def predict_fp(
     reste_navaids = navaids[start_index:]
     point_depart = _Point(
         lat=cast(
-            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+            Position, cast(Flight, flight.before(hole.start)).at_ratio(1)
         ).latitude,
         lon=cast(
-            Position, cast(Flight, flight.before(trou.start)).at_ratio(1)
+            Position, cast(Flight, flight.before(hole.start)).at_ratio(1)
         ).longitude,
         name=start_nav_name,
     )
-    new_timestamp = trou.start
+    new_timestamp = hole.start
     for navaid in reste_navaids:
         dmin = distance(
             point_depart.latitude,
@@ -71,14 +81,13 @@ def predict_fp(
             navaid.longitude,
         )
         t = int(dmin / gs)
-        # new_timestamp = trou.start + timedelta(seconds=t)
         new_timestamp = new_timestamp + timedelta(seconds=t)
         point_depart = navaid
         data_points["latitude"].append(navaid.latitude)
         data_points["longitude"].append(navaid.longitude)
         data_points["timestamp"].append(new_timestamp)
-        # compute difference between trou.start and new_timestamp
-        time_difference_seconds = (new_timestamp - trou.start).total_seconds()
+        # compute difference between hole.start and new_timestamp
+        time_difference_seconds = (new_timestamp - hole.start).total_seconds()
         time_difference_minutes = time_difference_seconds / 60
         if time_difference_minutes > minutes and len(data_points["timestamp"]) > 1:
             break
@@ -87,7 +96,7 @@ def predict_fp(
         **data_points,
         "icao24": flight.icao24,
         "callsign": flight.callsign,
-        "altitude": cast(Position, flight.at(trou.start)).altitude,
+        "altitude": cast(Position, flight.at(hole.start)).altitude,
         "flight_id": flight.flight_id,
     }
     return Flight(pd.DataFrame(new_columns)).resample("1s").first(minutes * 60 + 1)
