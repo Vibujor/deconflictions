cd30b1019059d3d2e19e63922a465fab41817bed
Vibujor <enceladael@gmail.com>
2023-11-28 13:24:00 +0000

    doc + parsing

--- format_data.py
+++ format_data.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import argparse
 from pathlib import Path
 from traffic.data import (  # noqa: F401
     aixm_airspaces,
@@ -22,6 +23,15 @@ def preprocess_data(
     extent: str,
     altitude_min: int = 20000,
 ) -> None:
+    """
+    Formats trajectory data based on metadata.
+
+    :param trajectory_data: Path to trajectory data file
+    :param metadata_file: Path to metadata file
+    :param output_file: Path to output file
+    :param extent: Extent
+    :param altitude_min: Minimum altitude
+    """
     # read_parquet accepts files or folders
     # metadata = pd.read_parquet("A2207_old.parquet")
     metadata = pd.read_parquet(metadata_file)
@@ -34,7 +44,7 @@ def preprocess_data(
     concatenated_df = pd.read_parquet(trajectory_data)
 
     # first filter on the data
-    # we drop unground value (useless)
+    # we drop onground value (useless)
     t = (
         Traffic(concatenated_df)
         .drop(columns=["onground"])
@@ -59,4 +69,28 @@ def preprocess_data(
 
 
 if __name__ == "__main__":
-    preprocess_data()
+    parser = argparse.ArgumentParser(description="Preprocess trajectory data.")
+
+    parser.add_argument(
+        "trajectory_data", type=Path, help="Path to the trajectory data file"
+    )
+    parser.add_argument("metadata_file", type=Path, help="Path to the metadata file")
+    parser.add_argument("output_file", type=Path, help="Path to the output file")
+    parser.add_argument("extent", type=str, help="Extent parameter, for example LFBBDX")
+    parser.add_argument(
+        "--altitude_min",
+        type=int,
+        default=20000,
+        help="Minimum altitude (default: 20000)",
+    )
+
+    args = parser.parse_args()
+
+    # Call the preprocess_data function directly with the parsed arguments
+    preprocess_data(
+        trajectory_data=args.trajectory_data,
+        metadata_file=args.metadata_file,
+        output_file=args.output_file,
+        extent=args.extent,
+        altitude_min=args.altitude_min,
+    )
